ac2e60e03390b6c04189bda8331f388e41676139
Danielle <daniellebarry-gass@DANIELLEs-Air.attlocal.net>
2015-10-10 17:51:44 -0500

    radians and degrees in math

--- math.js
+++ math.js
@@ -4,3 +4,9 @@ rad.clamp = function(v,min,max) {
 rad.remap = function(v,l1,h1,l2,h2){
 	return l2 + (v - l1) * (h2 - l2) / (h1 - l1);
 }
+rad.degtorad = function(d) {
+    return d*(3.14159/180);
+}
+rad.radtodeg = function(r){
+    return r*(180/3.14159)
+}
