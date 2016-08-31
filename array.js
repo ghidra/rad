501fa2ba046113d675b927a8de7d822aa095d73a
ghidra <ghidra@gmail.com>
2016-08-31 15:54:11 -0400

    remove from array

--- array.js
+++ array.js
@@ -3,3 +3,10 @@ rad.flusharray=function(a){
         a.pop();
     }
 }
+//http://stackoverflow.com/questions/5767325/how-to-remove-a-particular-element-from-an-array-in-javascript
+rad.removefromarray=function(a,elem){
+	var i = a.indexOf(elem);
+	if (i > -1) {
+    	a.splice(i, 1);
+	}
+}
