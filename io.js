d140b3f130c7dd16bbef850b578a0c8bbb369f43
ghidra <ghidra@gmail.com>
2022-06-10 22:16:43 -0400

    adding more sql stuff to io

--- io.js
+++ io.js
@@ -2,24 +2,24 @@
 //requires rad.ajax
 
 rad.io=function(id,path,callback){
-	path = path || "";
+	this.path = path || "";
 	callback = callback || function fallback(){console.log("empty callback")};
 	this.storage_type = "local";
 	this.a = new rad.ajax();
 
-	return this.init(id,path,callback);
+	return this.init(id,callback);
 }
-rad.io.prototype.init=function(id,path,callback){
+rad.io.prototype.init=function(id,callback){
 	//first see if we have access to a database...
-	if(path!=""){
-		lo = path;
+	if(this.path!=""){
+		lo = this.path;
 		//a = new rad.ajax();
 		_this = this;///pass the reference to this for access in ajax callback
 		this.a.get(
-			path,
+			_this.path,
 			"q=login",
 			function(lamda){
-				_this.storage_type = "mysql";
+				//_this.storage_type = "mysql";
 				callback(lamda);
 			}
 		);
@@ -31,28 +31,48 @@ rad.io.prototype.init=function(id,path,callback){
 
 	return this;
 }
+rad.io.prototype.set_storage_type=function(t){
+	this.storage_type = t;
+}
 //---------------------
+///return a list of files from local storage
+// or if we are logged in, from the data base
 rad.io.prototype.list=function(){
 	//get list of saved
-
-	//this is the brute force local storage way
-	var files = this.storage.getobj(this.id);
-	if(!files){//there are no files already saved
-		return null;
-	}else{
-		var file_names=[];
-		for (n in files){
-			file_names.push(n);
+	console.log("just trying to list: "+this.storage_type);
+	if(this.storage_type == "local"){
+		//this is the brute force local storage way
+		var files = this.storage.getobj(this.id);
+		if(!files){//there are no files already saved
+			return ["no files found"];
+		}else{
+			var file_names=[];
+			for (n in files){
+				file_names.push(n);
+			}
+			return file_names;
 		}
-		return file_names;
 	}
-	//console.log(files);
+	if(this.storage_type == "mysql"){
+		//we goona need to do some ajax in here
+		///get all the files from mysql
+		console.log('---we need to go akax into the database');
+		_this=this;
+		this.a.get(
+			_this.path,
+			"q=list",
+			function(lamda){
+				console.log(lamda);
+			}
+		);
+		return ["WAITING"];
+
+	}
+	console.log("ERROR IN RAD.IO.LIST");
+	return ["ERROR"];
+	
 }
 rad.io.prototype.save=function(name,src,sanitize){//optional sanitize function to clean out the save data if need be
-	//save the file
-	//if using local storage get the file object first, to add to it
-	var files = this.storage.getobj(this.id);
-	
 	//clean data
 	var src_clean;
 	if(typeof sanitize === "function"){
@@ -61,25 +81,41 @@ rad.io.prototype.save=function(name,src,sanitize){//optional sanitize function t
 	if(src_clean===undefined) src_clean=src;
 	//var src_clean=this.sanitize_script(src);
 
-	if(!files){//there are no files already saved
-		var new_file = {};
-		new_file[name]=src_clean;
-		//console.log(name)
-		this.storage.setobj(this.id,new_file);
-	}else{
-		//there are saved files, lets append, or overwrite
-		files[name]=src_clean;
-		this.storage.setobj(this.id,files);
+	//save the file
+	if(this.storage_type == "local"){
+		//if using local storage get the file object first, to add to it
+		var files = this.storage.getobj(this.id);
+
+		if(!files){//there are no files already saved
+			var new_file = {};
+			new_file[name]=src_clean;
+			//console.log(name)
+			this.storage.setobj(this.id,new_file);
+		}else{
+			//there are saved files, lets append, or overwrite
+			files[name]=src_clean;
+			this.storage.setobj(this.id,files);
+		}
+	}
+	if(this.storage_type == "mysql"){
+		//we goona need to do some ajax in here
+		///go see if we have this file already, if se we can overwrite, if not, we just save
+		//look at the the already loaded files... so we can overwrite that way..maybe
 	}
 }
 rad.io.prototype.load=function(name){
 	//load file, return object to be processed
-	//get all the local files
-	var files = this.storage.getobj(this.id);
-	if(files[name]){
-		return files[name];//return the specfic file requested
-	}else{
-		return 'none';
+	if(this.storage_type == "local"){
+		//get all the local files
+		var files = this.storage.getobj(this.id);
+		if(files[name]){
+			return files[name];//return the specfic file requested
+		}else{
+			return 'none';
+		}
+	}
+	if(this.storage_type == "mysql"){
+		//we goona need to do some ajax in here
 	}
 }
 
