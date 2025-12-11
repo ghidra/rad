8c1e6459d9526a75bf88ef772e3308d1ee6e297b
ghidra <ghidra@gmail.com>
2025-12-11 01:15:35 -0500

    more shadow work in chainsaw

--- chainsaw.js
+++ chainsaw.js
@@ -178,6 +178,159 @@ rad.chainsaw=class{
 			callback();
 		}
 	}
+	createShadowTexture(size=2048){
+		this.shadowTexSize=size;
+		this.shadowTex = this.gl.createTexture();
+		this.shadowVolumeMat4 = this.orthoMat4(-size, size,-size, size,0.1, 50.0);
+		
+		this.gl.bindTexture(this.gl.TEXTURE_2D, this.shadowTex);
+		this.gl.texImage2D(
+			this.gl.TEXTURE_2D,
+			0,
+			this.gl.DEPTH_COMPONENT16,
+			size, size, // shadow resolution
+			0,
+			this.gl.DEPTH_COMPONENT,
+			this.gl.UNSIGNED_SHORT,
+			null
+		);
+		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.NEAREST);
+		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MAG_FILTER, this.gl.NEAREST);
+		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+		this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_WRAP_T, this.gl.CLAMP_TO_EDGE);
+
+		//attach it to a frame buffer.. this might need it's own function call later?
+
+		this.shadowFBO = this.gl.createFramebuffer();
+		
+		//this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.shadowFBO);
+		this.gl.framebufferTexture2D(
+			this.gl.FRAMEBUFFER,
+			this.gl.DEPTH_ATTACHMENT,
+			this.gl.TEXTURE_2D,
+			this.shadowTex,
+			0
+		);
+		//this.gl.drawBuffers([]); // no color buffer
+	}
+	//https://github.com/toji/gl-matrix/blob/master/src/mat4.js
+	lookatMatrix(eye=new rad.vector3(0.0,0.0,1.0), center=new rad.vector3(0.0,0.0,0.0), up=new rad.vector3(0.0,1.0,0.0)){
+		let out = new Float32Array(16);
+
+		let x0, x1, x2, y0, y1, y2, z0, z1, z2, len;
+		let eyex = eye.x;
+		let eyey = eye.y;
+		let eyez = eye.z;
+		let upx = up.x;
+		let upy = up.y;
+		let upz = up.z;
+		let centerx = center.x;
+		let centery = center.y;
+		let centerz = center.z;
+
+		if (
+			Math.abs(eyex - centerx) < glMatrix.EPSILON &&
+			Math.abs(eyey - centery) < glMatrix.EPSILON &&
+			Math.abs(eyez - centerz) < glMatrix.EPSILON
+		) {
+			return identity(out);
+		}
+
+		z0 = eyex - centerx;
+		z1 = eyey - centery;
+		z2 = eyez - centerz;
+
+		len = 1 / Math.sqrt(z0 * z0 + z1 * z1 + z2 * z2);
+		z0 *= len;
+		z1 *= len;
+		z2 *= len;
+
+		x0 = upy * z2 - upz * z1;
+		x1 = upz * z0 - upx * z2;
+		x2 = upx * z1 - upy * z0;
+		len = Math.sqrt(x0 * x0 + x1 * x1 + x2 * x2);
+		if (!len) {
+			x0 = 0;
+			x1 = 0;
+			x2 = 0;
+		} else {
+			len = 1 / len;
+			x0 *= len;
+			x1 *= len;
+			x2 *= len;
+		}
+
+		y0 = z1 * x2 - z2 * x1;
+		y1 = z2 * x0 - z0 * x2;
+		y2 = z0 * x1 - z1 * x0;
+
+		len = Math.sqrt(y0 * y0 + y1 * y1 + y2 * y2);
+		if (!len) {
+			y0 = 0;
+			y1 = 0;
+			y2 = 0;
+		} else {
+			len = 1 / len;
+			y0 *= len;
+			y1 *= len;
+			y2 *= len;
+		}
+
+		out[0] = x0;
+		out[1] = y0;
+		out[2] = z0;
+		out[3] = 0;
+		out[4] = x1;
+		out[5] = y1;
+		out[6] = z1;
+		out[7] = 0;
+		out[8] = x2;
+		out[9] = y2;
+		out[10] = z2;
+		out[11] = 0;
+		out[12] = -(x0 * eyex + x1 * eyey + x2 * eyez);
+		out[13] = -(y0 * eyex + y1 * eyey + y2 * eyez);
+		out[14] = -(z0 * eyex + z1 * eyey + z2 * eyez);
+		out[15] = 1;
+
+  		return out;
+	}
+	orthoMat4(left, right, bottom, top, near, far){
+		let out = new Float32Array(16);
+		const lr = 1 / (left - right);
+		const bt = 1 / (bottom - top);
+		const nf = 1 / (near - far);
+		out[0] = -2 * lr;
+		out[1] = 0;
+		out[2] = 0;
+		out[3] = 0;
+		out[4] = 0;
+		out[5] = -2 * bt;
+		out[6] = 0;
+		out[7] = 0;
+		out[8] = 0;
+		out[9] = 0;
+		out[10] = 2 * nf;
+		out[11] = 0;
+		out[12] = (left + right) * lr;
+		out[13] = (top + bottom) * bt;
+		out[14] = (far + near) * nf;
+		out[15] = 1;
+		return out;
+	}
+	drawShadowPass(shadowProgram){
+		this.gl.bindFramebuffer(this.gl.FRAMEBUFFER, this.shadowFBO);
+		this.gl.viewport(0, 0, this.shadowTexSize, this.shadowTexSize);
+		this.gl.clear(this.gl.DEPTH_BUFFER_BIT);
+
+		// Use shadow program
+		this.gl.useProgram(shadowProgram);
+
+		// Set uLightViewProj
+		//this.gl.uniformMatrix4fv(lightVPUniform, false, lightViewProj);
+
+		//drawSceneDepthOnly();
+	}
 }
 //
 rad.chainsaw.shader=class{
